c63b7ca8a7422d3b51b1cff6904a72c6e018c738
KostadinovK <kostadinovk2001@gmail.com>
2019-08-15 15:11:17 +0300

    Finished home page and header logic

--- scripts/controllers/homeController.js
+++ scripts/controllers/homeController.js
@@ -1,5 +1,9 @@
 const homeController = function(){
     const getHome = function(context){
+
+        const loggedIn = storage.getData('userInfo') !== null;
+        context.loggedIn = loggedIn;
+
         context.loadPartials({
             header: './views/common/header.hbs',
             footer: './views/common/footer.hbs'
